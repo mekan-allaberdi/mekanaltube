81e55583a5769d0c406e4591f9b29c19059a5335
mekan.allaberdi <mekan.allaberdi@gmail.com>
2020-11-15 00:54:41 +0500

    changes 6

--- mytube/web/app.py
+++ mytube/web/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from view.video import bp as video_bp
-from view.theme import bp as theme_bp
+from mytube.web.view.video import bp as video_bp
+from mytube.web.view.theme import bp as theme_bp
 
 app.register_blueprint(video_bp, url_prefix='')
 app.register_blueprint(theme_bp, url_prefix='/theme')
